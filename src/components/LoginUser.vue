433441eb3266b81da8a6c35e0695d38127147d14
Gabriel Torres de Almeida <66974496+GabrielTorresDev95@users.noreply.github.com>
2025-04-21 21:17:36 -0300

    Update LoginUser.vue

--- src/components/LoginUser.vue
+++ src/components/LoginUser.vue
@@ -36,7 +36,7 @@ export default {
     async loginAdmin() {
       this.error = null;
       try {
-        const response = await axios.post(`https://cliente-c9lqgiw84-gabriels-projects-a95fe2cc.vercel.app/`, {
+        const response = await axios.post(`https://cliente-c9lqgiw84-gabriels-projects-a95fe2cc.vercel.app`, {
           username: this.username,
           password: this.password,
         });
