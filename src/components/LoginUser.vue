1622e32c3fff70fd378162f318f98268a6f80049
Gabriel Torres de Almeida <66974496+GabrielTorresDev95@users.noreply.github.com>
2025-04-22 22:19:13 -0300

    Update LoginUser.vue

--- src/components/LoginUser.vue
+++ src/components/LoginUser.vue
@@ -36,7 +36,7 @@ export default {
     async loginAdmin() {
       this.error = null;
       try {
-        const response = await axios.post(`https://cliente-5.onrender.com/admin/login`, {
+        const response = await axios.post(`https://cliente-5.onrender.com`, {
           username: this.username,
           password: this.password,
         });
