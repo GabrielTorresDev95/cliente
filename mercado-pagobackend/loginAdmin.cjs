5ac84ad91fe0a79bbc3e2b8a4bc5c00a08ddd191
Gabriel Torres de Almeida <66974496+GabrielTorresDev95@users.noreply.github.com>
2025-05-05 13:28:35 -0300

    Update loginAdmin.cjs

--- mercado-pagobackend/loginAdmin.cjs
+++ mercado-pagobackend/loginAdmin.cjs
@@ -19,7 +19,7 @@ const pool = mysql.createPool({
 });
 
 // 🔐 Login de Administrador
-router.post('/admin/login', async (req, res) => {
+router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
