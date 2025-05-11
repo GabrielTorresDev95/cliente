ea262185db275b5a4b037095ff9a52b06c2d79b9
Gabriel Torres de Almeida <66974496+GabrielTorresDev95@users.noreply.github.com>
2025-05-11 18:46:42 -0300

    Update LoginUser.vue

--- src/components/LoginUser.vue
+++ src/components/LoginUser.vue
@@ -38,7 +38,7 @@ export default {
   console.log("Tentando logar com:", this.username);
       
       try {
-        const response = await axios.post(`${API_URL}/login`, {
+        const response = await axios.post(`https://cliente-4.onrender.com/login`, {
           username: this.username,
           password: this.password,
         });
